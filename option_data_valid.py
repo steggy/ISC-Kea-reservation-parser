35c3cf9630d01fcf1a4017232c48a4c5cb9e2843
Steggy <steggy@gmail.com>
2024-04-19 17:24:33 -0700

    Add all option-data fields for our uses to the module that checks and creates fields if missing in record

--- option_data_valid.py
+++ option_data_valid.py
@@ -1,13 +1,15 @@
 
 
 
-
+# check for option-data - if not create
 def valid_option_data(records,num):
+    optdata = [['host-name',12],['domain-name-servers',6],['domain-name',15],['broadcast-address',28],['subnet-mask',1],['routers',3]] 
     obj = list(enumerate(records['Dhcp4']['reservations']))
     if 'option-data' in list(obj[num])[1]:
         print(list(obj[num])[1])
-        if not next((item for item in list(obj[num])[1]['option-data'] if "name" in item and item["name"] == 'host-name'), None):
-            list(obj)[num][1]['option-data'].append({'space': 'dhcp4', 'name': 'host-name', 'code': 12, 'data': ''})
+        for i in optdata:
+            if not next((item for item in list(obj[num])[1]['option-data'] if "name" in item and item["name"] == i[0]), None):
+                list(obj)[num][1]['option-data'].append({'space': 'dhcp4', 'name': 'host-name', 'code': i[1], 'data': ''})
         if 'user-context' not in list(obj[num])[1]['option-data']:
             print('No Context')
             list(obj[num])[1]['option-data'].append({'user-context':{'comment':'','last-modified':'','description':''}})
@@ -24,6 +26,11 @@ def valid_option_data(records,num):
         print('NO OPTION-DATA')
         print(list(obj)[num][1])
         list(obj)[num][1]['option-data'] = [{'space': 'dhcp4', 'name': 'host-name', 'code': 12, 'data': ''}]
+        list(obj)[num][1]['option-data'].append({"space": "dhcp4","name": "domain-name-servers","code": 6,"data": ""})
+        list(obj)[num][1]['option-data'].append({"space": "dhcp4","name": "domain-name","code": 15,"data": "syntax.lo"})
+        list(obj)[num][1]['option-data'].append({"space": "dhcp4","name": "broadcast-address","code": 28,"data": ""})
+        list(obj)[num][1]['option-data'].append({"space": "dhcp4","name": "subnet-mask","code": 1,"data": ""})
+        list(obj)[num][1]['option-data'].append({"space": "dhcp4","name": "routers","code": 3,"data": ""})
         list(obj)[num][1]['option-data'].append({'user-context':{'comment':'','last-modified':'','description':''}})
         print(list(obj)[num][1])
     #res = records
